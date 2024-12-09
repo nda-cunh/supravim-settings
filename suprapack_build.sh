0ae03a9309f5d35e0434de9b7882435871bf31be
Nda-cunh <hydraldev@gmail.com>
2024-12-09 04:39:46 +0100

    update suprapack.sh

--- suprapack_build.sh
+++ suprapack_build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 git clone https://gitlab.com/nda-cunh/SupraVim.wiki.git $MESON_INSTALL_DESTDIR_PREFIX/share/supravim-gui
+pushd ..
 suprapack build $MESON_INSTALL_DESTDIR_PREFIX
+popd

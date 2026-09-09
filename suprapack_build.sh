cce218a2adeb7b32b3bbed8b6ca5a6cce7c862b7
nda-cunh <hydraldev@gmail.com>
2026-09-10 01:53:16 +0200

    chore(build): gate suprapack info generation behind option

--- suprapack_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone https://gitlab.com/nda-cunh/SupraVim.wiki.git $MESON_INSTALL_DESTDIR_PREFIX/share/supravim-gui --depth 1
-pushd ..
-suprapack build $MESON_INSTALL_DESTDIR_PREFIX
-popd
